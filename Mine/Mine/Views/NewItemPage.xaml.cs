5ac292788c22dd8d02c319ebb5b721f807b3e907
Benjamin Agrelius <bagrelius@seattleu.edu>
2021-01-23 21:31:51 -0800

    dded Stepper for Value Attribute on NewItemPage”

--- Mine/Mine/Views/NewItemPage.xaml.cs
+++ Mine/Mine/Views/NewItemPage.xaml.cs
@@ -38,5 +38,15 @@ namespace Mine.Views
         {
             await Navigation.PopModalAsync();
         }
+    
+        /// <summary>
+        /// Udates the Display Value when the Stepper changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            ValueValue.Text = String.Format("{0}", e.NewValue);
+        }
     }
 }
