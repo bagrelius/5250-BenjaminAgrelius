37275e116035abaccd1f48556ca618711df96811
Benjamin Agrelius <bagrelius@seattleu.edu>
2021-01-27 20:54:50 -0800

    Added Today’s DateTime to AboutPage, changed AboutPage.xaml, AboutPage.xaml.cs

--- Mine/Mine/Views/AboutPage.xaml.cs
+++ Mine/Mine/Views/AboutPage.xaml.cs
@@ -13,6 +13,10 @@ namespace Mine.Views
         public AboutPage()
         {
             InitializeComponent();
+
+            CurrentDateTimeLabel.Text = System.DateTime.Now.ToString("MM/dd/yy hh:mm:ss");
+
         }
+
     }
 }
