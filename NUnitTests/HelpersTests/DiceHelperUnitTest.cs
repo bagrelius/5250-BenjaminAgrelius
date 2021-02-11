7e85ab8fc920e23cd285c104ad9abbeef406569d
Benjamin Agrelius <bagrelius@seattleu.edu>
2021-02-10 19:49:56 -0800

    Added RollDice_Valid_Roll_3_Dice_10_Fixed_5_Should_Return_5 to DiceHelperUnitTests.cs

--- NUnitTests/HelpersTests/DiceHelperUnitTest.cs
+++ NUnitTests/HelpersTests/DiceHelperUnitTest.cs
@@ -100,5 +100,21 @@ namespace NUnitTests.HelpersTests
 
         }
 
+        [Test]
+        public void RollDice_Valid_Roll_3_Dice_10_Fixed_5_Should_Return_5()
+        {
+            //Arrange
+            DiceHelper.ForcedRandomValue = 5;
+            DiceHelper.ForceRollsToNotRandom = true;
+
+            //Act
+            var result = DiceHelper.RollDice(1, 10);
+            //Reset
+            DiceHelper.ForceRollsToNotRandom = false;
+            //Assert
+            Assert.AreEqual(15, result);
+
+        }
+
     }
 }
