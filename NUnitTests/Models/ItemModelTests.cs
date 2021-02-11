6f285413a84739eaccacf808766c3c318e7fac6e
Benjamin Agrelius <bagrelius@seattleu.edu>
2021-02-10 19:04:38 -0800

    Updated UT to use new ItemModel in ItemModelTests.cs

--- NUnitTests/Models/ItemModelTests.cs
+++ NUnitTests/Models/ItemModelTests.cs
@@ -9,19 +9,19 @@ namespace NUnitTests.Models
     class ItemModelTests
     {
         [Test]
-        public void ItemModel_Constructor_Valid()
+        public void ItemModel_Constructor_Valid_Default_Should_Pass()
         {
             //Arrange
 
 
             //Act
-
+            var result = new ItemModelTests();
 
             //Reset
 
 
             //Assert
-            Assert.Pass();
+            Assert.IsNotNull(result);
         }
     }
 }
